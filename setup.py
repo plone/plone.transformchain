8c74fe0578fce2cbec2bbe9e6564cc9661558460
Philip Bauer <bauer@starzel.de>
2018-09-19 09:24:55 +0200

    add classifiers

--- setup.py
+++ setup.py
@@ -22,6 +22,8 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords='zope2 repoze transform',
