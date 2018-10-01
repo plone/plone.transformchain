e95d5f8b3ac33934395b497d44194caf8f879a49
Maurits van Rees <maurits@vanrees.org>
2018-10-01 17:41:40 +0200

    Added Plone 5.2 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         "Framework :: Plone",
         "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
