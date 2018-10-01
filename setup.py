f4edb7efe448c62e3c555ae017c05bec34e5e174
Maurits van Rees <maurits@vanrees.org>
2018-10-01 18:03:58 +0200

    Removed unneeded exclude ez_setup.py from setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     author_email='optilude@gmail.com',
     url='https://pypi.org/project/plone.transformchain',
     license='BSD',
-    packages=find_packages(exclude=['ez_setup']),
+    packages=find_packages(),
     namespace_packages=['plone'],
     include_package_data=True,
     zip_safe=False,
